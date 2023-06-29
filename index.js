32945734cf8383560d19ba3735bf261d37c5405c
AndreTooGood <andrejohnv@gmail.com>
2023-06-29 11:56:15 +0530

    index.js pushed

---
+++ index.js
@@ -0,0 +1,24 @@
+var spin = new TimelineMax();
+var loading = new TimelineMax({
+    repeat: -1
+});
+
+spin.to($('.blade'), 0.6, {
+    rotation: 360,
+    repeat: -1,
+    transformOrigin: '50% 50%',
+    ease: Linear.easeNone
+});
+
+loading.to($('.loadingText'), 1, {
+        opacity: 0,
+        ease: Linear.easeNone
+    })
+    .to($('.loadingText'), 1, {
+        opacity: 1,
+        ease: Linear.easeNone
+    });
+
+  $(window).load(function() {
+ 		$("#pre-loader").fadeOut("slow");
+ 	});
